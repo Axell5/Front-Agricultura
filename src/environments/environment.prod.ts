1fe66188da42fbd3c3a289da4a7a3a0ab63d7340
Axel Nicolas Caro Rojas <157514583+Axell5@users.noreply.github.com>
2025-06-02 17:03:41 -0500

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.agriprecision.com',
   wsUrl: 'wss://api.agriprecision.com/ws',
-  payuPublicKey: 'TNt331BACO6x4rE8NDsitKaMYK',
-  payuMerchantId: 'JnzN3ExacO9U6P3',
-  payuAccountId: 'PK78c9750349I08X1IddX5I70b',
-  payuApiKey: '4Vj8eK4rloUd272L48hsrarnUA'
+  payuPublicKey: '4Vj8eK4rloUd272L48hsrarnUAK',
+  payuMerchantId: '508029',
+  payuAccountId: '512321',
 };
