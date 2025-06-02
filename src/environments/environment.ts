01235d567156ac72c3c9f831ed681bdced2919d9
Axel Nicolas Caro Rojas <157514583+Axell5@users.noreply.github.com>
2025-06-02 17:08:39 -0500

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000',
   wsUrl: 'ws://localhost:3000/ws',
-  payuPublicKey: 'YOUR_PAYU_PUBLIC_KEY',
+  payuPublicKey: '4Vj8eK4rloUd272L48hsrarnUA',
   payuMerchantId: '508029',
   payuAccountId: '512321',
   payuApiKey: '4Vj8eK4rloUd272L48hsrarnUA'
