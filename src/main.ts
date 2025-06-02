2599d3ca84e4069845e8cda998e59c3205636158
Axel Nicolas Caro Rojas <157514583+Axell5@users.noreply.github.com>
2025-06-02 15:16:47 -0500

    Fix HTTP interceptors registration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
@@ -20,7 +20,15 @@ bootstrapApplication(AppComponent, {
       ReactiveFormsModule
     ),
     AuthGuard,
-    AuthInterceptor,
-    ErrorInterceptor
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
   ]
 }).catch(err => console.error(err));
